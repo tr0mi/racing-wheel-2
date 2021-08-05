7d626e2e7b10b6e9a27979167a37e8ebbe4047da
tr0mi <davidtroman97@gmail.com>
2021-08-05 21:45:13 +0100

    Updates

---
+++ precache-manifest.e4a10bc63c4b3fcc7d697df8adeab67b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a9a4fb5baafaa856759b38a3b04002",
+    "url": "/racing-wheel-2/index.html"
+  },
+  {
+    "revision": "514ebfd149eb823e9ede",
+    "url": "/racing-wheel-2/static/css/main.d05d0d56.chunk.css"
+  },
+  {
+    "revision": "a7035bbbe61efa25a3b2",
+    "url": "/racing-wheel-2/static/js/2.95fcc08c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/racing-wheel-2/static/js/2.95fcc08c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514ebfd149eb823e9ede",
+    "url": "/racing-wheel-2/static/js/main.813dca3f.chunk.js"
+  },
+  {
+    "revision": "737401e8507cc5d76530",
+    "url": "/racing-wheel-2/static/js/runtime-main.9c68aa21.js"
+  }
+]);
